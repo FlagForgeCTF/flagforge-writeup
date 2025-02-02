cdaadbdb71418806cbaf25b2ed5f642170bec0a3
aryan4859 <aryan.shrestha@presidnetial.edu.np>
2025-02-02 15:06:46 -0500

    init

--- Components/Footer.js
+++ Components/Footer.js
@@ -9,14 +9,14 @@ function Footer() {
       <div className="bg-indigo-600 dark:bg-indigo-900">
         <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
           <p className="text-gray-50 text-sm text-center sm:text-left">
-            © {year} C0de Chronicles —
+            © {year} FlagForge —
             <a
-              href="https://twitter.com/"
+              href="https://flagforge.xyz/"
               rel="noopener noreferrer"
               className="text-gray-50 ml-1"
               target="_blank"
             >
-              @Aryanstha
+              @flagforge
             </a>
           </p>
           <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
